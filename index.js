b77c72137e7bb3cffb0fb4b41e115ee2fce72dc3
Aleksey Osipov <alexey11rus@gmail.com>
2020-04-10 20:27:10 +0300

    feat: add data, sass, html, js

---
+++ index.js
@@ -0,0 +1,14 @@
+import { cardsData } from "./modules/cards";
+
+class Main {
+  constructor() {
+    this.currentCathegory = "action1";
+    this.cards = this.loadCards();
+  }
+
+  loadCards() {
+    
+  }
+
+
+}
---
+++ modules/cards.js
@@ -0,0 +1,468 @@
+const cardsData = {
+  cathegories: {
+    action1: ["cards", "dance", "dive", "draw", "tofish", "fly", "hug", "jump"],
+    action2: ["open", "play", "point", "ride", "run", "sing", "skip", "swim"],
+    action3: ["argue", "build", "carry", "catch", "drive", "drop", "pull", "push"],
+    adjective: ["big", "small", "fast", "slow", "friendly", "unfriendly", "young", "old"],
+    animals1: ["cat", "chick", "chicken", "dog", "horse", "pig", "rabbit", "sheep"],
+    animals2: ["bird", "fish", "frog", "giraffe", "lion", "mouse", "turtle", "dolphin"],
+    clothes: ["skirt", "pants", "blouse", "dress", "boot", "shirt", "coat", "shoe"],
+    emotion: ["sad", "angry", "happy", "tired", "surprised", "scared", "smile", "laugh"]
+  },
+
+  //action 1
+  cry: {
+    ru: "плакать",
+    en: "cry",
+    img: "cry.jpg",
+    mp3: "cry.mp3"
+  },
+
+  dance: {
+    ru: "танцевать",
+    en: "dance",
+    img: "dance.jpg",
+    mp3: "dance.mp3"
+  },
+
+  dive: {
+    ru: "нырять",
+    en: "dive",
+    img: "dive.jpg",
+    mp3: "dive.mp3"
+  },
+
+  draw: {
+    ru: "рисовать",
+    en: "draw",
+    img: "draw.jpg",
+    mp3: "draw.mp3"
+  },
+
+  tofish: {
+    ru: "ловить рыбу",
+    en: "fish",
+    img: "tofish.jpg",
+    mp3: "tofish.mp3"
+  },
+
+  fly: {
+    ru: "летать",
+    en: "fly",
+    img: "fly.jpg",
+    mp3: "fly.mp3"
+  },
+
+  hug: {
+    ru: "обнимать",
+    en: "hug",
+    img: "hug.jpg",
+    mp3: "hug.mp3"
+  },
+  
+  jump: {
+    ru: "прыгать",
+    en: "jump",
+    img: "jump.jpg",
+    mp3: "jump.mp3"
+  },
+
+  //action 2
+  open: {
+    ru: "открывать",
+    en: "open",
+    img: "open.jpg",
+    mp3: "open.mp3"
+  },
+
+  play: {
+    ru: "играть",
+    en: "play",
+    img: "play.jpg",
+    mp3: "play.mp3"
+  },
+
+  point: {
+    ru: "указывать",
+    en: "point",
+    img: "point.jpg",
+    mp3: "point.mp3"
+  },
+
+  ride: {
+    ru: "ездить",
+    en: "ride",
+    img: "ride.jpg",
+    mp3: "ride.mp3"
+  },
+
+  run: {
+    ru: "бегать",
+    en: "run",
+    img: "run.jpg",
+    mp3: "run.mp3"
+  },
+
+  sing: {
+    ru: "петь",
+    en: "sing",
+    img: "sing.jpg",
+    mp3: "sing.mp3"
+  },
+
+  skip: {
+    ru: "прыгать, пропускать",
+    en: "skip",
+    img: "skip.jpg",
+    mp3: "skip.mp3"
+  },
+
+  swim: {
+    ru: "плавать",
+    en: "swim",
+    img: "swim.jpg",
+    mp3: "swim.mp3"
+  },
+
+  //action 3
+  argue: {
+    ru: "спорить",
+    en: "argue",
+    img: "argue.jpg",
+    mp3: "argue.mp3"
+  },
+
+  build: {
+    ru: "строить",
+    en: "build",
+    img: "build.jpg",
+    mp3: "build.mp3"
+  },
+
+  carry: {
+    ru: "нести",
+    en: "carry",
+    img: "carry.jpg",
+    mp3: "carry.mp3"
+  },
+
+  catch: {
+    ru: "ловить",
+    en: "catch",
+    img: "catch.jpg",
+    mp3: "catch.mp3"
+  },
+
+  drive: {
+    ru: "водить машину",
+    en: "drive",
+    img: "drive.jpg",
+    mp3: "drive.mp3"
+  },
+
+  drop: {
+    ru: "падать",
+    en: "drop",
+    img: "drop.jpg",
+    mp3: "drop.mp3"
+  },
+
+  pull: {
+    ru: "тянуть",
+    en: "pull",
+    img: "pull.jpg",
+    mp3: "pull.mp3"
+  },
+
+  push: {
+    ru: "толкать",
+    en: "pull",
+    img: "pull.jpg",
+    mp3: "pull.mp3"
+  },
+
+  //adjective
+  big: {
+    ru: "большой",
+    en: "big",
+    img: "big.jpg",
+    mp3: "big.mp3"
+  },
+
+  small: {
+    ru: "маленький",
+    en: "small",
+    img: "small.jpg",
+    mp3: "small.mp3"
+  },
+
+  fast: {
+    ru: "быстрый",
+    en: "fast",
+    img: "fast.jpg",
+    mp3: "fast.mp3"
+  },
+
+  slow: {
+    ru: "медленный",
+    en: "slow",
+    img: "slow.jpg",
+    mp3: "slow.mp3"
+  },
+
+  friendly: {
+    ru: "дружелюбный",
+    en: "friendly",
+    img: "friendly.jpg",
+    mp3: "friendly.mp3"
+  },
+
+  unfriendly: {
+    ru: "недружелюбный",
+    en: "unfriendly",
+    img: "unfriendly.jpg",
+    mp3: "unfriendly.mp3"
+  },
+
+  young: {
+    ru: "молодой",
+    en: "young",
+    img: "young.jpg",
+    mp3: "young.mp3"
+  },
+
+  old: {
+    ru: "старый",
+    en: "old",
+    img: "old.jpg",
+    mp3: "old.mp3"
+  },
+
+  //animals 1
+  cat: {
+    ru: "кошка",
+    en: "cat",
+    img: "cat.jpg",
+    mp3: "cat.mp3"
+  },
+
+  chick: {
+    ru: "ципленок",
+    en: "chick",
+    img: "chick.jpg",
+    mp3: "chick.mp3"
+  },
+
+  chicken: {
+    ru: "ципленок",
+    en: "chicken",
+    img: "chicken.jpg",
+    mp3: "chicken.mp3"
+  },
+
+  dog: {
+    ru: "собака",
+    en: "dog",
+    img: "dog.jpg",
+    mp3: "dog.mp3"
+  },
+
+  horse: {
+    ru: "лошадь",
+    en: "horse",
+    img: "horse.jpg",
+    mp3: "horse.mp3"
+  },
+
+  pig: {
+    ru: "свинья",
+    en: "pig",
+    img: "pig.jpg",
+    mp3: "pig.mp3"
+  },
+
+  rabbit: {
+    ru: "кролик",
+    en: "rabbit",
+    img: "rabbit.jpg",
+    mp3: "rabbit.mp3"
+  },
+
+  sheep: {
+    ru: "овца",
+    en: "sheep",
+    img: "sheep.jpg",
+    mp3: "sheep.mp3"
+  },
+
+  //animal 2
+  bird: {
+    ru: "птица",
+    en: "bird",
+    img: "bird.jpg",
+    mp3: "bird.mp3"
+  },
+
+  fish: {
+    ru: "рыба",
+    en: "fish",
+    img: "fish.jpg",
+    mp3: "fish.mp3"
+  },
+
+  frog: {
+    ru: "лягушка",
+    en: "frog",
+    img: "frog.jpg",
+    mp3: "frog.mp3"
+  },
+
+  giraffe: {
+    ru: "жираф",
+    en: "giraffe",
+    img: "giraffe.jpg",
+    mp3: "giraffe.mp3"
+  },
+
+  lion: {
+    ru: "лев",
+    en: "lion",
+    img: "lion.jpg",
+    mp3: "lion.mp3"
+  },
+
+  mouse: {
+    ru: "мышь",
+    en: "mouse",
+    img: "mouse.jpg",
+    mp3: "mouse.mp3"
+  },
+
+  turtle: {
+    ru: "черепаха",
+    en: "turtle",
+    img: "turtle.jpg",
+    mp3: "turtle.mp3"
+  },
+
+  dolphin: {
+    ru: "дельфин",
+    en: "dolphin",
+    img: "dolphin.jpg",
+    mp3: "dolphin.mp3"
+  },
+
+  //clothes
+  skirt: {
+    ru: "юбка",
+    en: "skirt",
+    img: "skirt.jpg",
+    mp3: "skirt.mp3"
+  },
+
+  pants: {
+    ru: "брюки",
+    en: "pants",
+    img: "pants.jpg",
+    mp3: "pants.mp3"
+  },
+
+  blouse: {
+    ru: "блузка",
+    en: "blouse",
+    img: "blouse.jpg",
+    mp3: "blouse.mp3"
+  },
+
+  dress: {
+    ru: "платье",
+    en: "dress",
+    img: "dress.jpg",
+    mp3: "dress.mp3"
+  },
+
+  boot: {
+    ru: "ботинок",
+    en: "boot",
+    img: "boot.jpg",
+    mp3: "boot.mp3"
+  },
+
+  shirt: {
+    ru: "рубашка",
+    en: "shirt",
+    img: "shirt.jpg",
+    mp3: "shirt.mp3"
+  },
+
+  coat: {
+    ru: "пальто",
+    en: "coat",
+    img: "coat.jpg",
+    mp3: "coat.mp3"
+  },
+
+  shoe: {
+    ru: "туфли",
+    en: "shoe",
+    img: "shoe.jpg",
+    mp3: "shoe.mp3"
+  },
+
+  //emotions
+  sad: {
+    ru: "грустный",
+    en: "sad",
+    img: "sad.jpg",
+    mp3: "sad.mp3"
+  },
+
+  angry: {
+    ru: "сердитый",
+    en: "angry",
+    img: "angry.jpg",
+    mp3: "angry.mp3"
+  },
+
+  happy: {
+    ru: "счастливый",
+    en: "happy",
+    img: "happy.jpg",
+    mp3: "happy.mp3"
+  },
+
+  tired: {
+    ru: "уставший",
+    en: "tired",
+    img: "tired.jpg",
+    mp3: "tired.mp3"
+  },
+
+  surprised: {
+    ru: "удивлённый",
+    en: "surprised",
+    img: "surprised.jpg",
+    mp3: "surprised.mp3"
+  },
+
+  scared: {
+    ru: "испуганный",
+    en: "scared",
+    img: "scared.jpg",
+    mp3: "scared.mp3"
+  },
+
+  smile: {
+    ru: "улыбка",
+    en: "smile",
+    img: "smile.jpg",
+    mp3: "smile.mp3"
+  },
+
+  laugh: {
+    ru: "смех",
+    en: "laugh",
+    img: "laugh.jpg",
+    mp3: "laugh.mp3"
+  }
+};
