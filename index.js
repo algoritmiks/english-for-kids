760efcf35561e2491ec10d662df5379ddb0fafe1
Aleksey Osipov <alexey11rus@gmail.com>
2020-04-16 12:19:54 +0300

    feat: eslint add

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import { cardsData } from "./modules/cards.js";
+import { cardsData } from './modules/cards';
 
-const $ = c => document.querySelector(c);
-const $All = c => document.querySelectorAll(c);
-const scoreContainer = $(".score-container");
+const $ = (c) => document.querySelector(c);
+const $All = (c) => document.querySelectorAll(c);
+const scoreContainer = $('.score-container');
 
 class Card {
   constructor(card, cathegory) {
     this.img = card.img;
-    if (cathegory === "main") {
+    if (cathegory === 'main') {
       this.cathegoryID = cardsData.menu[card.en][0];
       this.cathegoryName = cardsData.menu[card.en][1];
       this.addMenuCardToDOM();
@@ -20,72 +20,72 @@ class Card {
   }
 
   addMenuCardToDOM() {
-    //menu container
+    // menu container
     const menuContainer = document.createElement('div');
-    menuContainer.classList = "menu-container";
+    menuContainer.classList = 'menu-container';
     menuContainer.dataset.name = this.cathegoryID;
     cards.appendChild(menuContainer);
 
-    //menu card
+    // menu card
     const menuCard = document.createElement('div');
-    menuCard.classList = "menu-card";
+    menuCard.classList = 'menu-card';
     menuContainer.appendChild(menuCard);
 
     const img = document.createElement('div');
-    img.classList = "menu-card__img";
+    img.classList = 'menu-card__img';
     img.style.backgroundImage = `url('./../../assets/img/${this.img}')`;
     menuCard.appendChild(img);
 
     const descr = document.createElement('div');
-    descr.classList = "menu-card__description";
+    descr.classList = 'menu-card__description';
     descr.innerText = this.cathegoryName;
     menuCard.appendChild(descr);
   }
 
   addCardToDOM() {
-    //card scene
+    // card scene
     const cardScene = document.createElement('div');
-    cardScene.classList = "card-scene";
+    cardScene.classList = 'card-scene';
     cards.appendChild(cardScene);
 
-    //card container
+    // card container
     const cardContainer = document.createElement('div');
-    cardContainer.classList = "card-container";
+    cardContainer.classList = 'card-container';
     cardContainer.dataset.name = this.en;
     cardScene.appendChild(cardContainer);
 
-    //back card
+    // back card
     const cardBack = document.createElement('div');
-    cardBack.classList = "card card__back";
+    cardBack.classList = 'card card__back';
     cardContainer.appendChild(cardBack);
 
     const imgBack = document.createElement('div');
-    imgBack.classList = "card__img";
+    imgBack.classList = 'card__img';
     imgBack.style.backgroundImage = `url('./../../assets/img/${this.img}')`;
     cardBack.appendChild(imgBack);
 
     const descriptionBack = document.createElement('div');
-    descriptionBack.classList = "card__description";
+    descriptionBack.classList = 'card__description';
     descriptionBack.innerText = this.ru;
     cardBack.appendChild(descriptionBack);
 
-    //front card
+    // front card
     const cardFront = document.createElement('div');
-    cardFront.classList = "card";
+    cardFront.classList = 'card';
     cardContainer.appendChild(cardFront);
 
     const imgFront = document.createElement('div');
-    imgFront.classList = "card__img";
+    imgFront.classList = 'card__img';
     imgFront.style.backgroundImage = `url('./../../assets/img/${this.img}')`;
     cardFront.appendChild(imgFront);
 
     const descriptionFront = document.createElement('div');
-    descriptionFront.classList = "card__description";
+    descriptionFront.classList = 'card__description';
     descriptionFront.innerText = this.en;
     cardFront.appendChild(descriptionFront);
 
     const rotate = document.createElement('div');
-    rotate.classList = "rotate";
+    rotate.classList = 'rotate';
     descriptionFront.appendChild(rotate);
 
     this.cardDOM = cardContainer;
@@ -94,7 +94,7 @@ class Card {
 
 class CardsDesk {
   constructor() {
-    this.currentCathegory = "main";
+    this.currentCathegory = 'main';
     this.cards = [];
     this.isModeGameActive = false;
     this.isGameStarted = false;
@@ -105,21 +105,21 @@ class CardsDesk {
     const cathegoryItems = cardsData.cathegories[this.currentCathegory];
     cathegoryItems.forEach((card) => {
       this.cards.push(new Card(cardsData[card], this.currentCathegory));
-    })
+    });
   }
 
   changeCathegory(cathegory) {
     this.currentCathegory = cathegory;
     this.cards = [];
-    cards.innerHTML = "";
+    cards.innerHTML = '';
     this.loadCards();
   }
 
   setStartButtonStatus() {
-    if (this.currentCathegory !== "main" && this.isModeGameActive) {
-      $(".start-btn").classList.remove("start-btn_hidden");
+    if (this.currentCathegory !== 'main' && this.isModeGameActive) {
+      $('.start-btn').classList.remove('start-btn_hidden');
     } else {
-      $(".start-btn").classList.add("start-btn_hidden");
+      $('.start-btn').classList.add('start-btn_hidden');
     }
   }
 
@@ -130,16 +130,16 @@ class CardsDesk {
   }
 
   setCardsGameMode() {
-    $All(".card").forEach(card => {
-      card.firstElementChild.classList.add("card__img_play");
-      card.lastElementChild.classList.add("card__description_play");
+    $All('.card').forEach((card) => {
+      card.firstElementChild.classList.add('card__img_play');
+      card.lastElementChild.classList.add('card__description_play');
     });
   }
 
   setCardsTrainingMode() {
-    $All(".card").forEach(card => {
-      card.firstElementChild.classList.remove("card__img_play");
-      card.lastElementChild.classList.remove("card__description_play");
+    $All('.card').forEach((card) => {
+      card.firstElementChild.classList.remove('card__img_play');
+      card.lastElementChild.classList.remove('card__description_play');
     });
   }
 
@@ -157,23 +157,22 @@ class CardsDesk {
   }
 
   addRightSingToScoreContainer() {
-    scoreContainer.innerHTML += `<div class="score-item score-item_right"></div>`;
+    scoreContainer.innerHTML += '<div class="score-item score-item_right"></div>';
   }
 
   addWrongSingToScoreContainer() {
-    scoreContainer.innerHTML += `<div class="score-item score-item_wrong"></div>`;
+    scoreContainer.innerHTML += '<div class="score-item score-item_wrong"></div>';
   }
-
 }
 
-const cards = $(".cards");
+const cards = $('.cards');
 const cardsDesk = new CardsDesk();
-const sound = $(".mp3");
+const sound = $('.mp3');
 
 
 const clickOnCardHandler = (e) => {
-  if (e.target.className !== "cards") {
-    let menuCard = e.target.closest(".menu-container");
+  if (e.target.className !== 'cards') {
+    const menuCard = e.target.closest('.menu-container');
     if (menuCard) {
       const selectedCathegory = menuCard.dataset.name;
       cardsDesk.changeCathegory(selectedCathegory);
@@ -181,33 +180,33 @@ const clickOnCardHandler = (e) => {
       cardsDesk.changeCardsMode();
     }
 
-    const card = e.target.closest(".card-container");
+    const card = e.target.closest('.card-container');
 
     if (!cardsDesk.isModeGameActive) {
-      const rotate = e.target.closest(".rotate");
+      const rotate = e.target.closest('.rotate');
 
       if (rotate) {
-        card.classList.add("rotate_click");
+        card.classList.add('rotate_click');
       }
-      if (card && !rotate && !card.classList.contains("rotate_click")) {
+      if (card && !rotate && !card.classList.contains('rotate_click')) {
         sound.src = `./assets/mp3/${card.dataset.name}.mp3`;
         sound.play();
       }
     }
 
     if (cardsDesk.isModeGameActive && cardsDesk.isGameStarted) {
-
-      if (card.dataset.name === currentCard.en && !card.classList.contains("card-container_done")) {
-        card.classList.add("card-container_done");
-        sound.src = `./assets/mp3/right.mp3`;
+      if (card.dataset.name === currentCard.en && !card.classList.contains('card-container_done')) {
+        card.classList.add('card-container_done');
+        sound.src = './assets/mp3/right.mp3';
         sound.play();
         cardsDesk.addRightSingToScoreContainer();
         continueGame();
       }
-      if (card.dataset.name !== currentCard.en && !card.classList.contains("card-container_done")) {
+
+      if (card.dataset.name !== currentCard.en && !card.classList.contains('card-container_done')) {
         cardsDesk.addWrongSingToScoreContainer();
         currentGameErrors += 1;
-        sound.src = `./assets/mp3/wrong.mp3`;
+        sound.src = './assets/mp3/wrong.mp3';
         sound.play();
       }
     }
@@ -217,11 +216,10 @@ const clickOnCardHandler = (e) => {
 cards.addEventListener('click', clickOnCardHandler);
 
 
-
 const moveMouseOutCardHandler = (e) => {
-  if (!e.target.closest(".card-container")) {
-    $All(".card-container").forEach(card => {
-      card.classList.remove("rotate_click");
+  if (!e.target.closest('.card-container')) {
+    $All('.card-container').forEach((card) => {
+      card.classList.remove('rotate_click');
     });
   }
 };
@@ -230,47 +228,47 @@ window.addEventListener('mousemove', moveMouseOutCardHandler);
 
 
 const openSideMenu = () => {
-  $(".hamburger").classList.add("hamburger_opened");
-  $(".menu-wrapper").classList.add("menu-wrapper_open");
-  $(".menu-shadow").classList.add("menu-shadow_active");
-  document.body.style.overflow = "hidden";
-}
+  $('.hamburger').classList.add('hamburger_opened');
+  $('.menu-wrapper').classList.add('menu-wrapper_open');
+  $('.menu-shadow').classList.add('menu-shadow_active');
+  document.body.style.overflow = 'hidden';
+};
 
 const closeSideMenu = () => {
-  $(".hamburger").classList.remove("hamburger_opened");
-  $(".menu-wrapper").classList.remove("menu-wrapper_open");
-  $(".menu-shadow").classList.remove("menu-shadow_active");
-  document.body.style.overflow = "visible";
-}
+  $('.hamburger').classList.remove('hamburger_opened');
+  $('.menu-wrapper').classList.remove('menu-wrapper_open');
+  $('.menu-shadow').classList.remove('menu-shadow_active');
+  document.body.style.overflow = 'visible';
+};
 
 const clickHamburgerHandler = () => {
-  if ($(".menu-wrapper").classList.contains("menu-wrapper_open")) {
+  if ($('.menu-wrapper').classList.contains('menu-wrapper_open')) {
     closeSideMenu();
   } else {
     openSideMenu();
   }
-}
+};
 
-$(".hamburger").addEventListener('click', clickHamburgerHandler);
+$('.hamburger').addEventListener('click', clickHamburgerHandler);
 
-$(".menu-shadow").addEventListener('click', (e) => {
-  if (e.target.classList.contains("menu-shadow_active")) {
+$('.menu-shadow').addEventListener('click', (e) => {
+  if (e.target.classList.contains('menu-shadow_active')) {
     closeSideMenu();
   }
 });
 
 
 const changeActiveMenu = (current) => {
-  $All(".menu-item").forEach(el => {
-    el.classList.remove("menu-item_active");
+  $All('.menu-item').forEach((el) => {
+    el.classList.remove('menu-item_active');
     if (el === current) {
-      el.classList.add("menu-item_active");
+      el.classList.add('menu-item_active');
     }
   });
 };
 
 const clickMenuHandle = (e) => {
-  if (e.target.tagName === "LI") {
+  if (e.target.tagName === 'LI') {
     cardsDesk.changeCathegory(e.target.dataset.name);
     changeActiveMenu(e.target);
     cardsDesk.changeCardsMode();
@@ -281,44 +279,43 @@ const clickMenuHandle = (e) => {
   }
 };
 
-$(".menu-wrapper").addEventListener('click', clickMenuHandle);
-
+$('.menu-wrapper').addEventListener('click', clickMenuHandle);
 
 
 const clickSwitcherHandle = (e) => {
   cardsDesk.changeGameModeActive();
   if (cardsDesk.isModeGameActive) {
-    e.target.classList.add("switcher_on");
+    e.target.classList.add('switcher_on');
   } else {
-    e.target.classList.remove("switcher_on");
+    e.target.classList.remove('switcher_on');
     if (cardsDesk.isGameStarted) {
       stopGame();
-    };
+    }
   }
-}
+};
 
-$(".switcher").addEventListener('click', clickSwitcherHandle);
+$('.switcher').addEventListener('click', clickSwitcherHandle);
 
 const shuffle = (arr) => {
   arr.sort(() => Math.random() - 0.5);
   return arr;
-}
+};
 
 let currentCards = [];
 let currentCard = {};
 let currentGameErrors = 0;
 
 const stopGame = () => {
-  $(".start-btn").classList.remove("repeat");
+  $('.start-btn').classList.remove('repeat');
   currentCard = {};
   currentCards = [];
   currentGameErrors = 0;
-  scoreContainer.innerHTML = "";
-  $All(".card-container").forEach((el) => {
-    el.classList.remove("card-container_done");
+  scoreContainer.innerHTML = '';
+  $All('.card-container').forEach((el) => {
+    el.classList.remove('card-container_done');
   });
   cardsDesk.changeGameStarted();
-}
+};
 
 const continueGame = (timeout = 1000) => {
   setTimeout(() => {
@@ -328,37 +325,36 @@ const continueGame = (timeout = 1000) => {
       sound.play();
     }
   }, timeout);
-}
+};
 
 const finishGame = () => {
   let timeOut = 5000;
-  $(".switcher").classList.remove("switcher_on");
+  $('.switcher').classList.remove('switcher_on');
   cardsDesk.changeGameModeActive();
-  $(".game-finish").classList.remove("game-finish_hidden");
+  $('.game-finish').classList.remove('game-finish_hidden');
 
   if (currentGameErrors > 0) {
-    $(".game-finish").classList.add("game-finish_loser");
-    $(".game-finish").dataset.errors = `Total ${currentGameErrors} errors.`;
-    sound.src = "./assets/mp3/loser.mp3";
+    $('.game-finish').classList.add('game-finish_loser');
+    $('.game-finish').dataset.errors = `Total ${currentGameErrors} errors.`;
+    sound.src = './assets/mp3/loser.mp3';
     sound.play();
   } else {
-    $(".game-finish").classList.add("game-finish_winner");
+    $('.game-finish').classList.add('game-finish_winner');
     timeOut = 3000;
-    sound.src = "./assets/mp3/winner.mp3";
+    sound.src = './assets/mp3/winner.mp3';
     sound.play();
   }
   stopGame();
 
   setTimeout(() => {
-    $(".game-finish").classList.add("game-finish_hidden");
-    $(".game-finish").classList.remove("game-finish_winner");
-    $(".game-finish").classList.remove("game-finish_loser");
-    $(".game-finish").dataset.errors = "";
-    cardsDesk.changeCathegory("main");
-    changeActiveMenu($(`[data-name="main"]`));
+    $('.game-finish').classList.add('game-finish_hidden');
+    $('.game-finish').classList.remove('game-finish_winner');
+    $('.game-finish').classList.remove('game-finish_loser');
+    $('.game-finish').dataset.errors = '';
+    cardsDesk.changeCathegory('main');
+    changeActiveMenu($('[data-name="main"]'));
   }, timeOut);
-
-}
+};
 
 
 const getNextCard = () => {
@@ -374,22 +370,22 @@ const runNewGame = () => {
   currentCards = [...cardsDesk.cards];
   shuffle(currentCards);
   continueGame(0);
-}
+};
 
 
 const repeatCurrentWord = () => {
   sound.src = `./assets/mp3/${currentCard.mp3}`;
   sound.play();
-}
+};
 
 
-const clickStartButton = (e) => {
+const clickStartButton = () => {
   if (!cardsDesk.isGameStarted) {
-    $(".start-btn").classList.add("repeat");
+    $('.start-btn').classList.add('repeat');
     runNewGame();
   } else {
     repeatCurrentWord();
   }
-}
+};
 
-$(".start-btn").addEventListener('click', clickStartButton);
+$('.start-btn').addEventListener('click', clickStartButton);
